8f497dd5c3ec353015e8c3ff93c3b6ab7a0fe3ae
Joshua Barringer <joshuabarringer@Joshua-Barringer.local>
2018-01-17 13:18:59 -0800

    updated player input tests

--- inputTest2.cpp
+++ inputTest2.cpp
@@ -26,17 +26,24 @@ void playerInput (userClass player) {
   	istream_iterator<string> iit (cin);   // stdin iterator
 
   	if (iit!=eos) playerOne.verb=*iit;
-
+  	if (iit==eos) {
+  		playerOne.playerNoun=0;
+  		playerControl (playerOne);
+  	}
   	++iit;
   	if (iit!=eos) playerOne.noun=*iit;
   	playerControl (playerOne);
+
 }
 
 void playerControl (userClass player) {
 
-	string verb[] = {"RUN", "WALK", "GO"};
+	string verb[] = {"RUN", "WALK", "GO", "HEAD"};
 	string south[] = {"SOUTH", "S",};
-	for (int i = 0; i < 3; i++ ) {
+	string north[] = {"NORTH", "N"};
+	string west[] = {"WEST", "W"};
+	string east[] = {"EAST", "E"};
+	for (int i = 0; i < 4; i++ ) {
 		size_t found = verb[i].find(player.verb);
 		if (found!=string::npos) {
 			playerOne.playerVerb=1;
@@ -48,7 +55,34 @@ void playerControl (userClass player) {
 	for (int i = 0; i < 2; i++ ) {
 		size_t found = south[i].find(player.noun);
 		if (found!=string::npos) {
+			playerOne.playerNoun=3;
+		}
+		if (i > 3) {
+			break;
+		}	
+	}
+	for (int i = 0; i < 2; i++ ) {
+		size_t found = north[i].find(player.noun);
+		if (found!=string::npos) {
 			playerOne.playerNoun=1;
+		}
+		if (i > 3) {
+			break;
+		}	
+	}
+	for (int i = 0; i < 2; i++ ) {
+		size_t found = east[i].find(player.noun);
+		if (found!=string::npos) {
+			playerOne.playerNoun=2;
+		}
+		if (i > 3) {
+			break;
+		}	
+	}
+	for (int i = 0; i < 2; i++ ) {
+		size_t found = west[i].find(player.noun);
+		if (found!=string::npos) {
+			playerOne.playerNoun=4;
 		}
 		if (i > 3) {
 			break;
