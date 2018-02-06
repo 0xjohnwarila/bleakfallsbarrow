518fbedf6148dcda79d957017c230119bd540b11
JohnNWarila <jwarila@localhost.localdomain>
2018-02-05 19:30:13 -0800

    Added spells, death choice, and some minor improvements to balancing. Changed some namespace issues.

--- combatModules/mainCombat.cpp
+++ combatModules/mainCombat.cpp
@@ -7,8 +7,7 @@
 #include <chrono>
 #include <thread>
 
-using namespace std;
-
+using std::string;
 
 class playerInfo {
 public:
@@ -41,6 +40,7 @@ void combatUserAttackBasic();
 void combatUserWait();
 string combatEnemyChoice();
 void combatEnemyAttack();
+void combatUserSpellBasic();
 
 int main(){
 	clearScreen();
@@ -52,6 +52,9 @@ int main(){
 }
 
 void introCombatText(){ // Basic intro text
+	using std::cout;
+	using std::endl;
+
 	cout << "WELCOME TO THE COMBAT MODULE!!" << endl;
 	cout << "IN THIS BREIF DEMO YOU WILL CREATE A CHARACTER AND FIGHT A FOE" << endl;
 	cout << "LET'S BEGIN!!!!!!!!!" << endl ;
@@ -59,6 +62,8 @@ void introCombatText(){ // Basic intro text
 }
 
 void getPlayerInfo(){ // Ask the player to input the data about their character
+	using namespace std;
+
 	srand(time(NULL));
 
 	string nameIn;
@@ -114,10 +119,13 @@ void makeEnemy(){ // Fill the enemy class with data on a default enemy
 }
 
 void clearScreen(){ // Clear the screen and move curser to the upper left
+	using std::cout;
+
 	cout << "\033[2J\033[1;1H";
 }
 
 void bubbleSort(string *array, int size){ // Sort the command array so the search can work
+	using std::string;
 	bool swap;
 	string temp;
 
@@ -136,11 +144,14 @@ void bubbleSort(string *array, int size){ // Sort the command array so the searc
 }
 
 string stringSearch(string *array, int size){ // Check if the userCommand is in the string and then return it so the function can be called
+	using std::string;
+	using std::cin;
+	using std::cout;
+
 	string userCommand;
 	for(;;){
 		cout << ": ";
 		getline(cin, userCommand);
-		if(userCommand == "END") break;
 
 		int first = 0;
 		int last = size - 1;
@@ -162,6 +173,9 @@ string stringSearch(string *array, int size){ // Check if the userCommand is in 
 
 void combatInitPrompt(){ // Initial the combat with choice to fight or run, if 
 	                 //fight is chosen you will be sent to the combat proper, if run you will have a chance to escape
+	using std::cout;
+	using std::endl;
+
 	clearScreen();
 	string array[] = {"FIGHT", "RUN", "BUFFER"};
 	cout << "A SCORNED LOVER STANDS BEFORE ME WEILDING A SPIKED CUDGEL!" << endl;
@@ -182,8 +196,12 @@ void combatInitPrompt(){ // Initial the combat with choice to fight or run, if
 }
 
 void combatFightModule(){ // The user has chosen to fight, run all of the functions for combat
+	using std::string;
+	using std::cout;
+	using std::endl;
+
 	bool userTurn = true;
-	string actionArray[] = {"ATTACK", "WAIT", "BUFFER"};
+	string actionArray[] = {"ATTACK", "WAIT", "SPELL", "BUFFER", "BUFFER"};
 
 	cout << "I WILL FIGHT THIS ONE!" << endl;
 
@@ -191,11 +209,11 @@ void combatFightModule(){ // The user has chosen to fight, run all of the functi
 
 	cout << "THE SCORNED LOVER STEPS CLOSER!" << endl;
 	cout << endl << "WHAT DO I DO?" << endl;
-	cout << "-- ATTACK -- WAIT --" << endl;
+	cout << "-- ATTACK -- WAIT -- SPELL --" << endl;
 	while(enemyOne.enemyHealth > 0 && playerOne.playerHealth > 0){
 		while(userTurn == true){
-			bubbleSort(actionArray, 3);
-			string userAction = stringSearch(actionArray, 3);
+			bubbleSort(actionArray, 5);
+			string userAction = stringSearch(actionArray, 5);
 
 			if(userAction == "ATTACK"){
 				combatUserAttackBasic();
@@ -206,8 +224,16 @@ void combatFightModule(){ // The user has chosen to fight, run all of the functi
 					cout << "ENEMY HEALTH AT " << enemyOne.enemyHealth << endl;
 				}
 				userTurn = !userTurn;
-			}
-			else{
+			}else if(userAction == "SPELL"){
+				combatUserSpellBasic();
+				if(enemyOne.enemyHealth <= 0 ){
+					cout << "I HAVE INCINERATED HIM! HOORAY!" << endl;
+					break;
+				}else{
+					cout << "ENEMY HEALTH AT " << enemyOne.enemyHealth << endl;
+				}
+				userTurn = !userTurn;
+			}else{
 				combatUserWait();
 				userTurn = !userTurn;
 			}
@@ -219,6 +245,9 @@ void combatFightModule(){ // The user has chosen to fight, run all of the functi
 			if(enemyChoice == "ATTACK"){
 				combatEnemyAttack();
 			}
+			if(playerOne.playerHealth <= 0){
+				cout << "I HAVE BEEN STRUCK DOWN! BLEH!" << endl;
+			}
 			userTurn = !userTurn;
 		}
 	}
@@ -227,6 +256,9 @@ void combatFightModule(){ // The user has chosen to fight, run all of the functi
 }
 
 void combatRunModule(){ // The user has chosen to try to run away, roll for chance of run away
+	using std::cout;
+	using std::endl;
+
 	clearScreen();
 	cout << "I AM A COWARD AND RUN AWAY!" << endl;
 }
@@ -236,38 +268,108 @@ void sleepMilli(int x){ // Sleeps for X milliseconds
 }
 
 void combatUserAttackBasic(){ // Rolls damage for the user basic attack and applies damage to the enemy
+	using std::cout;
+	using std::endl;
+
+	srand(time(NULL));
+
+	int damageRoll = (rand() % 10);
+
+	if(playerOne.classNum == 1){
+		clearScreen();
+		cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- SPELL (CAST A POWERFULL SPELL AT THE ENEMY... PROBABLY) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl;
+		if(damageRoll < 1){
+			damageRoll = 0;
+		}else{
+			damageRoll -= 1;
+		}
+		cout << endl << "I HAVE SMASHED HIS HEAD FOR " << damageRoll << " DAMAGE!" << endl;
+		enemyOne.enemyHealth = enemyOne.enemyHealth - damageRoll;
+	}else if(playerOne.classNum == 2){
+                clearScreen();
+		cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- SPELL (CAST A POWERFULL SPELL AT THE ENEMY... PROBABLY) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl;
+		damageRoll += playerOne.playerLevel;
+		cout << endl << "I HAVE SMASHED HIS HEAD FOR " << damageRoll << " DAMAGE!" << endl;
+		enemyOne.enemyHealth = enemyOne.enemyHealth - damageRoll;
+        }else if(playerOne.classNum == 3){
+                clearScreen();
+		cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- SPELL (CAST A POWERFULL SPELL AT THE ENEMY... PROBABLY) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl;
+		damageRoll += 1;
+		cout << endl << "I HAVE SMASHED HIS HEAD FOR " << damageRoll << " DAMAGE!" << endl;
+		enemyOne.enemyHealth = enemyOne.enemyHealth - damageRoll;
+        }else if(playerOne.classNum == 4){
+                clearScreen();
+		cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- SPELL (CAST A POWERFULL SPELL AT THE ENEMY... PROBABLY) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl;
+		if(damageRoll < 4){
+			damageRoll = 0;
+		}else{
+			damageRoll -= 4;
+		}
+		cout << endl << "I HAVE SMASHED HIS HEAD FOR " << damageRoll << " DAMAGE!" << endl;
+		enemyOne.enemyHealth = enemyOne.enemyHealth - damageRoll;
+        }
+}
+
+void combatUserSpellBasic(){
+	using std::cout;
+	using std::endl;
+
 	srand(time(NULL));
 
 	int damageRoll;
 
 	if(playerOne.classNum == 1){
 		clearScreen();
-		cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl;
+		cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- SPELL (CAST A POWERFULL SPELL AT THE ENEMY... PROBABLY) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl << endl;
 		damageRoll = (rand() % 10);
-		cout << endl << "I HAVE SMASHED HIS HEAD FOR " << damageRoll << " DAMAGE!" << endl;
-		enemyOne.enemyHealth = enemyOne.enemyHealth - damageRoll;
+		if(damageRoll == 0){
+			cout << "I CAST A FIRE BALL AT THE ENEMY! IT GOES PAST THEM AND HITS ME IN THE BACK OF THE HEAD!" << endl;
+			cout << "I LOSE 3 HEALTH!" << endl;
+			playerOne.playerHealth = playerOne.playerHealth - 3;
+		}else if(damageRoll < 10){
+			cout << "I SEND A FIRE BALL AT THE ENEMY!" << endl;
+			cout << "IT CONNECTS WITH HIS LEFT NOSTRIL!" << endl;
+			enemyOne.enemyHealth = enemyOne.enemyHealth - 4;
+			cout << "HE TAKES 4 DAMAGE, AND HIS NOSE HAIR IS SINGED!" << endl;
+		}else if(damageRoll == 10){
+			cout << "I SEND A LIGHTNING BOLT AT THE ENEMY!" << endl;
+			cout << "IT STRIKES HIM IN THE RIGHT BIG TOE!" << endl;
+			enemyOne.enemyHealth = enemyOne.enemyHealth - 7;
+			cout << "THE PAIN OF A HUNDRED STUBBED TOES BRINGS HIM 7 DAMAGES!" << endl;
+		}
 	}else if(playerOne.classNum == 2){
                 clearScreen();
-		cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl;
-		damageRoll = (rand() % 10) + playerOne.playerLevel;
-		cout << endl << "I HAVE SMASHED HIS HEAD FOR " << damageRoll << " DAMAGE!" << endl;
-		enemyOne.enemyHealth = enemyOne.enemyHealth - damageRoll;
+		cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- SPELL (CAST A POWERFULL SPELL AT THE ENEMY... PROBABLY) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl << endl;
+		cout << "I TRY VERY HARD TO CAST A SPELL. INSTEAD I HAVE DROOLED ON MY CHEST!" << endl;
         }else if(playerOne.classNum == 3){
                 clearScreen();
-		cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl;
-		damageRoll = (rand() % 10);
-		cout << endl << "I HAVE SMASHED HIS HEAD FOR " << damageRoll << " DAMAGE!" << endl;
-		enemyOne.enemyHealth = enemyOne.enemyHealth - damageRoll;
+		cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- SPELL (CAST A POWERFULL SPELL AT THE ENEMY... PROBABLY) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl << endl;
+		cout << "I TRY VERY HARD TO CAST A SPELL. INSTEAD I HAVE DROOLED ON MY CHEST!" << endl;
         }else if(playerOne.classNum == 4){
                 clearScreen();
-		cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl;
+		cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- SPELL (CAST A POWERFULL SPELL AT THE ENEMY... PROBABLY) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl << endl;
 		damageRoll = (rand() % 10);
-		cout << endl << "I HAVE SMASHED HIS HEAD FOR " << damageRoll << " DAMAGE!" << endl;
-		enemyOne.enemyHealth = enemyOne.enemyHealth - damageRoll;
+		if(damageRoll == 0){
+			cout << "I CAST A FIRE BALL AT THE ENEMY! IT GOES PAST THEM AND HITS ME IN THE BACK OF THE HEAD!" << endl;
+			cout << "I LOSE 3 HEALTH!" << endl;
+			playerOne.playerHealth = playerOne.playerHealth - 3;
+		}else if(damageRoll < 10){
+			cout << "I SEND A FIRE BALL AT THE ENEMY!" << endl << endl;
+			cout << "IT CONNECTS WITH HIS LEFT NOSTRIL!" << endl << endl;
+			enemyOne.enemyHealth = enemyOne.enemyHealth - 5;
+			cout << "HE TAKES 5 DAMAGE, AND HIS NOSE HAIR IS SINGED!" << endl;
+		}else if(damageRoll == 10){
+			cout << "I SEND A LIGHTNING BOLT AT THE ENEMY!" << endl;
+			cout << "IT STRIKES HIM IN THE RIGHT BIG TOE!" << endl;
+			enemyOne.enemyHealth = enemyOne.enemyHealth - 8;
+			cout << "THE PAIN OF A HUNDRED STUBBED TOES BRINGS HIM 8 DAMAGE!" << endl;
+		}
         }
 }
-
 void combatUserWait(){ // User is an idiot and waited
+	using std::cout;
+	using std::endl;
+
 	clearScreen();
 	cout << "OPTIONS -- ATTACK (HIT THE ENEMY WITH A LARGE ROCK) -- WAIT (STAND WITH A BLANK EXPRESION)" << endl;
 	cout << endl << "I HAVE ACOMPLISHED MY LIFE LONG GOAL OF WASTING MY TURN!" << endl;
@@ -279,6 +381,10 @@ string combatEnemyChoice(){ // TODO : Make this choice semi random
 }
 
 void combatEnemyAttack(){ // Roll enemy damage and apply damage
+	using std::cout;
+	using std::endl;
+	using std::cin;
+
 	srand(time(NULL));
 
 	int damageRoll;
@@ -287,14 +393,33 @@ void combatEnemyAttack(){ // Roll enemy damage and apply damage
 		damageRoll = (rand() % 10);
 		if(damageRoll == 0){
 			
-			cout << "THE BASTARD HAS MISSED! NOW IS MY CHANCE!" << endl;
+			cout << endl << "THE BASTARD HAS MISSED! NOW IS MY CHANCE!" << endl;
 			cout << endl;
 		}else{
 			
 			playerOne.playerHealth = playerOne.playerHealth - damageRoll;
 			cout << endl << "OOFF, I'VE TAKEN " << damageRoll << " DAMAGE!" << endl;
-			cout << "I ONLY HAVE " << playerOne.playerHealth << " LEFT! I MUST BE CAREFUL" << endl;
-			cout << endl;
+			if(playerOne.playerHealth <= 0){
+				string userDeathCommand;
+				bool userDed;
+
+				cout << endl << "I MUST NOW CHOOSE TO HOLD ON OR DIE." << endl;
+				getline(cin, userDeathCommand);
+
+				while(!userDed){
+					if(userDeathCommand == "DIE"){
+						userDed = true;
+					}else{
+						cout << "I REALLY AM TIRED..." << endl << endl;
+						getline(cin, userDeathCommand);
+					}
+
+				}
+			}else{
+				cout << "I ONLY HAVE " << playerOne.playerHealth << " LEFT! I MUST BE CAREFUL" << endl;
+				cout << endl;
+			}
+			
 		}
 	}
 }
