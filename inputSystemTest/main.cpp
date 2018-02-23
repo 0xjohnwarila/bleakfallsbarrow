e82952993870604049643ac4048bfc6409815ec4
Joshua Barringer <jbarringer1999@gmail.com>
2018-02-22 22:27:45 -0800

    added dev mode with ./main dev

--- inputSystemTest/main.cpp
+++ inputSystemTest/main.cpp
@@ -24,18 +24,18 @@ int userInput::english = 0;
 
 //initializing player values
 
-std::string playerInfo::playerName = "JOSH, THE MAGNIFICENT";
-int playerInfo::playerAge = 18;
-int playerInfo::classNum = 1;
+std::string playerInfo::playerName = "null";
+int playerInfo::playerAge = 0;
+int playerInfo::classNum = 0;
 int userInput::playerLoc = 2;
-int playerInfo::playerHealth = 20;
+int playerInfo::playerHealth = 0;
 int playerInfo::playerLevel = 0;
 int playerInfo::battleStatus = 0;
 bool playerInfo::playerArthritis = false;
 bool playerInfo::playerCrit = false;
 
-bool userInput::greenRoomCheck = true;
-bool userInput::startRoomCheck = true;
+bool userInput::greenRoomCheck = false;
+bool userInput::startRoomCheck = false;
 
 //initializing startRoom values:
 
@@ -59,15 +59,48 @@ void startUp();
 void givePlayerHealth();
 void startGame();
 
-int main(){
+int main(int argc, char *argv[])
+{
 	using std::cout;
 	using std::endl;
+	using std::string;
+	using std::cin;
+	srand(time(NULL));
 
 	cout << "WELCOME TO THE COMBATMODULE INTEGRATION DEMO! YOU WILL CREATE A CHARACTER AND FIGHT AN ENEMY!" << endl;
 
 	//getPlayerInfo();
-	srand(time(NULL));
-	startGame();
+
+	if (argc == 2) {
+		std::string developerMode = argv[1];
+		if (developerMode == "dev") {
+			userInput::startRoomCheck = true;
+			userInput::greenRoomCheck = true;
+
+			playerInfo::playerName = "JOSH, THE MAGNIFICENT";
+			playerInfo::playerAge = 18;
+			playerInfo::classNum = 1;
+			userInput::playerLoc = 2;
+			playerInfo::playerHealth = 20;
+			
+			clear();
+			cout << "welcome to dev mode\n\nwhat room?: ";
+			string devRoom;
+			getline(cin, devRoom);
+			if (devRoom=="start") {
+				userInput::playerLoc = 2;
+			}
+			else if (devRoom=="green") {
+				userInput::playerLoc = 3;
+			}
+			startGame();
+		}
+	}
+	else {
+		getPlayerInfo();
+		startGame();
+	}
+
 	return 0;
 }
 
